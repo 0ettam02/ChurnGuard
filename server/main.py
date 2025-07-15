79e560a55756e8b856e782104b01a38ac3e8098d
0ettam02 <matteoaruta6@gmail.com>
2025-07-15 16:53:30 +0200

    prova2

--- server/main.py
+++ server/main.py
@@ -69,8 +69,7 @@ def datasets(path):
     df_noChurn = df_encoded[df_encoded["churn"] == 0]
 
     return df_encoded, df_noChurn
-
-df = datasets("dataset1.csv")
+df_encoded, df_noChurn = datasets("dataset1")
 # SEZIONE VERIFICA CHURN----------------------------------------------------------------------------------
 class InputUtente(BaseModel):
     eta: float
